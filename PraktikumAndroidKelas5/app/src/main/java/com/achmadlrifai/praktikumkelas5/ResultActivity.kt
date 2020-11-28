ce8ba9cbc70a78c8162966df2334365bcf974d69
Achmad Rifa'i <73744356+achmadlrifai@users.noreply.github.com>
2020-11-28 11:21:19 +0800

    Add files via upload

---
+++ PraktikumAndroidKelas5/app/src/main/java/com/achmadlrifai/praktikumkelas5/ResultActivity.kt
@@ -0,0 +1,34 @@
+package com.achmadlrifai.praktikumkelas5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    companion object{
+        final val EXTRA_NAME = "student_name"
+        final val EXTRA_ID = "student_id"
+        final val EXTRA_PHONE_NUMBER = "student_phone_number"
+        final val EXTRA_STUDENT = "student"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val tvStudentIdResult = findViewById<TextView>(R.id.tvStudentIdResult)
+        val tvStudentNameResult = findViewById<TextView>(R.id.tvStudentNameResult)
+        val tvStudentPhoneNumberResult = findViewById<TextView>(R.id.tvStudentPhoneNumberResult)
+
+//        val id = intent.getStringExtra(EXTRA_ID)
+//        val name = intent.getStringExtra(EXTRA_NAME)
+//        val phoneNumber = intent.getStringExtra(EXTRA_PHONE_NUMBER)
+
+        val student = intent.getParcelableExtra<Student>(EXTRA_STUDENT)
+
+        if (student != null) {
+            tvStudentIdResult.text = student.id
+            tvStudentNameResult.text = student.name
+            tvStudentPhoneNumberResult.text = student.phoneNumber
+        }
+    }
+}
---
+++ PraktikumAndroidKelas5/app/src/main/java/com/achmadlrifai/praktikumkelas5/StudentActivity.kt
@@ -0,0 +1,50 @@
+package com.achmadlrifai.praktikumkelas5
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class StudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student)
+
+        val btSendStudent = findViewById<Button>(R.id.btSendStudent)
+        val btDialStudent = findViewById<Button>(R.id.btDialStudent)
+        val etIdStudent = findViewById<EditText>(R.id.etIdStudent)
+        val etNameStudent = findViewById<EditText>(R.id.etNameStudent)
+        val etPhoneNumberStudent = findViewById<EditText>(R.id.etPhoneNumberStudent)
+
+
+        btSendStudent.setOnClickListener {
+
+            val id = etIdStudent.text.toString()
+            val name = etNameStudent.text.toString()
+            val phoneNumber = etPhoneNumberStudent.text.toString()
+
+            val student = Student(
+                    id,
+                    name,
+                    phoneNumber
+            )
+
+            //POJO Plain Old Java Object
+
+            val i = Intent(this,ResultActivity::class.java)
+//            i.putExtra(ResultActivity.EXTRA_ID,id)
+//            i.putExtra(ResultActivity.EXTRA_NAME,name)
+//            i.putExtra(ResultActivity.EXTRA_PHONE_NUMBER,phoneNumber)
+            i.putExtra(ResultActivity.EXTRA_STUDENT,student)
+            startActivity(i)
+        }
+
+        btDialStudent.setOnClickListener {
+            val phoneNumber = etPhoneNumberStudent.text.toString()
+            val i = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+            startActivity(i)
+        }
+    }
+}
